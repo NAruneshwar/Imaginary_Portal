edad25555dfd5647ee7a00a50ee7f1ee6c5a7c35
NAruneshwar <57510031+NAruneshwar@users.noreply.github.com>
2020-08-17 17:59:40 -0400

    update

--- resources/views/vendor/black-bits/laravel-cognito-auth/reset-password.blade.php
+++ resources/views/vendor/black-bits/laravel-cognito-auth/reset-password.blade.php
@@ -10,21 +10,6 @@
                     <div class="card-body">
                         <form method="POST" action="{{ route('password.request') }}" aria-label="{{ __('Reset Password') }}">
                             {{ csrf_field() }}
-
-                            <div class="form-group row">
-                                <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                                <div class="col-md-6">
-                                    <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ $email ?? old('email') }}" required autofocus>
-
-                                    @if ($errors->has('email'))
-                                        <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                    @endif
-                                </div>
-                            </div>
-
                             <div class="form-group row">
                                 <label for="token" class="col-md-4 col-form-label text-md-right">
                                     Token
